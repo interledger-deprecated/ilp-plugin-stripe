caee4641ed19f0a685f374353126418d00758049
Michiel de Jong <michiel@ripple.com>
2018-01-31 11:53:30 +0100

    mention test.js

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ilp-plugin-stripe
 Interledger plugin based on https://github.com/interledgerjs/ilp-plugin-btp and https://stripe.com/docs/api
+
+Save this as test.js:
+```js
+const Plugin = require('.')
+const plugin = new Plugin({
+  stripe: {
+     api_key: 'sk_test_sgmhnulE2YhvqafcHj5IMnhR',
+     currency: 'EUR',
+     source: 'tok_visa',
+     scale: 2,
+     description: 'Interledger settlement'
+  },
+  server: 'btp+wss://:token@amundsen.ilpdemo.org:1801'
+})
+console.log('connecting')
+plugin.connect().then(async () => {
+  console.log('connected')
+  await plugin.sendMoney(123)
+  console.log('sent')
+})
+```
+
+Then run:
+```sh
+npm install
+DEBUG=* node test.js
+```
+
--- index.js
+++ index.js
@@ -8,26 +8,25 @@ const debug = require('debug')('ilp-plugin-stripe')
 // {
 //   stripe: {
 //     api_key: 'sk_test_sgmhnulE2YhvqafcHj5IMnhR',
-//     currency: 'EUR',
+//     currency: 'EUR', // notice scale is always 2 except for https://stripe.com/docs/currencies#zero-decimal
 //     source: 'tok_visa',
-//     scale: 2,
 //     description: 'Interledger settlement'
 //   }
 // }
 class StripePlugin extends PluginBtp {
-  constructor ({ stripe }) {
-    super()
-    this.options = stripe
-    this.stripe = Stripe(this.options.api_key)
+  constructor (options) {
+    super(options)
+    this.options = options
+    this.stripe = Stripe(this.options.stripe.api_key)
   }
   async sendMoney (amount) {
-    await new Promise(resolve => {
-      debug('charge started', err, charge)
+    await new Promise((resolve, reject) => {
+      debug('charge started')
       this.stripe.charges.create({
-        amount: parseInt(amount) / Math.pow(10, (this.options.scale || 0)),
-        currency: (this.options.currency || 'EUR'),
-        source: (this.options.source || 'tok_visa'),
-        description: (this.options.description || 'Interledger settlement') + ' ' + new Date().toISOString()
+        amount: parseInt(amount),
+        currency: (this.options.stripe.currency || 'EUR'),
+        source: (this.options.stripe.source || 'tok_visa'),
+        description: (this.options.stripe.description || 'Interledger settlement') + ' ' + new Date().toISOString()
       }, function(err, charge) {
         debug('charge completed', err, charge)
         if (err) {
