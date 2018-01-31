7f8f829cd9dbd91ac9b354aa8e6ab234120fe7c5
Michiel de Jong <michiel@ripple.com>
2018-01-31 12:30:59 +0100

    start invert

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ const Plugin = require('.')
 const plugin = new Plugin({
   stripe: {
      api_key: 'sk_test_sgmhnulE2YhvqafcHj5IMnhR',
-     currency: 'EUR',
-     source: 'tok_visa',
-     scale: 2,
+     assetCode: 'EUR',
+     assetScale: 2, // notice scale is always 0 or 2, as per https://stripe.com/docs/currencies#zero-decimal
+     customer: 'cus_CEmxWWOoW2SVqh',
      description: 'Interledger settlement'
   },
   server: 'btp+wss://:token@amundsen.ilpdemo.org:1801'
@@ -23,8 +23,12 @@ plugin.connect().then(async () => {
 })
 ```
 
-Create a Stripe account, and read your test (sandbox) api key from
-Edit test.js to use your test (sandbox) api key on line 4.
+Create a Stripe account, and on https://dashboard.stripe.com/test/customers
+create a customer. To this customer, add a creditcard (you can use http://www.easy400.net/js2/regexp/ccnums.html).
+Take the ID of the customer you just created, and use it for `customer` on line 7 of test.js.
+On https://dashboard.stripe.com/account/apikeys click 'Reveal test key token' and use it for `api_key` on line 4 of test.js.
+Optionally pick a different description and asset code (but keep in mind some currencies use asset scale 0 instead of 2, see
+https://stripe.com/docs/currencies#presentment-currencies).
 
 Then run:
 ```sh
--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const PluginBtp = require('ilp-plugin-btp')
 const Stripe = require("stripe")
 const debug = require('debug')('ilp-plugin-stripe')
 
+const ZERO_SCALE_CURR = ['BIF', 'CLP', 'DJF', 'GNF', 'JPY', 'KMF', 'KRW', 'MGA', 'PYG', 'RWF', 'VND', 'VUV', 'XAF', 'XOF', 'XPF']
+
 // Options:
 // {
 //   stripe: {
 //     api_key: 'sk_test_sgmhnulE2YhvqafcHj5IMnhR',
-//     currency: 'EUR', // notice scale is always 2 except for https://stripe.com/docs/currencies#zero-decimal
-//     source: 'tok_visa',
+//     assetCode: 'EUR',
+//     assetScale: 2, // notice scale is always 2, we don't support https://stripe.com/docs/currencies#zero-decimal
+//     customer: 'cus_CEmxWWOoW2SVqh',
 //     description: 'Interledger settlement'
 //   }
 // }
@@ -18,21 +21,28 @@ class StripePlugin extends PluginBtp {
     super(options)
     this.options = options
     this.stripe = Stripe(this.options.stripe.api_key)
+    if (!options.stripe) { throw new Error('Missing options.stripe - try {}') }
+    if (!options.stripe.api_key) { throw new Error('Missing options.stripe.api_key - try "sk_test_sgmhnulE2YhvqafcHj5IMnhR"') }
+    if (!options.stripe.customer) { throw new Error('Missing options.stripe.customer - try "cus_CEmxWWOoW2SVqh"') }
+    if (!options.stripe.assetCode) { throw new Error('Missing options.stripe.assetCode - try "EUR" (and assetScale 0 or 2)') }
+    const expectedScale = (ZERO_SCALE_CURR.indexOf(options.stripe.assetCode) === -1 ? 2 : 0)
+    if (options.stripe.assetScale !== expectedScale) { throw new Error('The options.stripe.assetScale must equal 0 or 2, see https://stripe.com/docs/currencies#zero-decimal') }
+    if (!options.stripe.description) { throw new Error('Missing options.stripe.description - try "Interledger settlement"') }
   }
-  async sendMoney (amount) {
+  async handleMoney (from, {requestId, data }) {
     await new Promise((resolve, reject) => {
       debug('charge started')
       this.stripe.charges.create({
-        amount: parseInt(amount),
-        currency: (this.options.stripe.currency || 'EUR'),
-        source: (this.options.stripe.source || 'tok_visa'),
-        description: (this.options.stripe.description || 'Interledger settlement') + ' ' + new Date().toISOString()
+        amount: parseInt(data.amount),
+        currency: this.options.stripe.assetCode,
+        customer: this.options.stripe.customer,
+        description: this.options.stripe.description + ' ' + new Date().toISOString()
       }, function(err, charge) {
         debug('charge completed', err, charge)
         if (err) {
           reject(err)
         } else {
-          resolve()
+          resolve([])
         }
       })
     })
